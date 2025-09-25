78b3fe3c7d03acbced1fa204ebbcf75755d191a1
Hellnwo <hellenwo12@gmail.com>
2025-09-24 22:51:15 -0400

    config tweak

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/se_project_final/',
   server: {     
     port: 3000, // Change the port number to 3000
   },
