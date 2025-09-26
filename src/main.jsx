de53c7c11acea26965cecc4bb4a210b7b07dfa51
Hellnwo <hellenwo12@gmail.com>
2025-09-25 22:20:41 -0400

    BroswerRouter Tweak pt3

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter basename="/se_project_final" >
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>
